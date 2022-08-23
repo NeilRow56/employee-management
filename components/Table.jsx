035277ab28ca88e67a663bd4e8dc315496391421
Neil Rowland <neilrowland56@yahoo.com>
2022-08-23 18:55:23 +0100

    getting data from mongodb using react query

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import data from "../database/data.json"
+
 import { getUsers } from "../lib/helper";
+import { useQuery } from 'react-query'
 
 export default function Table(){
-    getUsers().then( res => console.log(res))
+
+    const { isLoading, isError, data, error } = useQuery('users', getUsers)
+
+    if(isLoading) return <div>Employee data is Loading...</div>;
+    if(isError) return <div>Got Error {error}</div>
+    
     return (
         <table className="min-w-full table-auto">
             <thead>
@@ -61,7 +67,7 @@ function Tr ({id, name, avatar, email, salary, date, status }) {
                         <span>{date || "Unknown"}</span>
                     </td>
                     <td className="px-16 py-2">
-                    <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
+                    <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
                     </td>
                     <td className="px-16 py-2 flex justify-around gap-5">
                         <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
